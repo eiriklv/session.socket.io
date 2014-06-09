e07b1dab8d28d19fbe2628e081d0367d2e8ce108
Nathan G. Romano <nathan.g.romano@gmail.com>
2014-06-09 12:56:19 -0400

    TypeError: Cannot call method 'pop' of null
    This would prevent the error.  The value will be undefined.  Or we could wrap it around a try/catch

--- session.socket.io.js
+++ session.socket.io.js
@@ -36,9 +36,9 @@ module.exports = function(io, sessionStore, cookieParser, key) {
   function findCookie(handshakeInput) {
     // fix for express 4.x (parse the cookie sid to extract the correct part)
     var handshake = JSON.parse(JSON.stringify(handshakeInput)); // copy of object
-    if(handshake.secureCookies && handshake.secureCookies[key]) handshake.secureCookies = handshake.secureCookies[key].match(/\:(.*)\./).pop();
-    if(handshake.signedCookies && handshake.signedCookies[key]) handshake.signedCookies[key] = handshake.signedCookies[key].match(/\:(.*)\./).pop();
-    if(handshake.cookies && handshake.cookies[key]) handshake.cookies[key] = handshake.cookies[key].match(/\:(.*)\./).pop();
+    if(handshake.secureCookies && handshake.secureCookies[key]) handshake.secureCookies = (handshake.secureCookies[key].match(/\:(.*)\./) || []).pop();
+    if(handshake.signedCookies && handshake.signedCookies[key]) handshake.signedCookies[key] = (handshake.signedCookies[key].match(/\:(.*)\./) || []).pop();
+    if(handshake.cookies && handshake.cookies[key]) handshake.cookies[key] = (handshake.cookies[key].match(/\:(.*)\./) ||).pop();
 
     // original code
     return (handshake.secureCookies && handshake.secureCookies[key])
