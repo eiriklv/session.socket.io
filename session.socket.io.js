dc9e9e879591764ed9dce0d374e58dfc6907dde2
Timothy Strimple <tim@tstrimple.com>
2014-12-11 01:34:57 -0600

    fixed security issue and cookie secret bug

--- session.socket.io.js
+++ session.socket.io.js
@@ -1,5 +1,3 @@
-var util = require('util');
-
 module.exports = function(io, sessionStore, cookieParser, key) {
   key = key || 'connect.sid';
 
@@ -15,7 +13,6 @@ module.exports = function(io, sessionStore, cookieParser, key) {
     return bind.call(this, event, callback, io.sockets);
   };
 
-  // fikse denne
   this.getSession = function(socket, callback) {
     cookieParser(socket.handshake, {}, function (parseErr){
       sessionStore.get(findCookie(socket.handshake), function (storeErr, session) {
@@ -33,14 +30,7 @@ module.exports = function(io, sessionStore, cookieParser, key) {
     }.bind(this));
   }
 
-  function findCookie(handshakeInput) {
-    // fix for express 4.x (parse the cookie sid to extract the correct part)
-    var handshake = JSON.parse(JSON.stringify(handshakeInput)); // copy of object
-    if(handshake.secureCookies && handshake.secureCookies[key]) handshake.secureCookies = (handshake.secureCookies[key].match(/\:(.*)\./) || []).pop();
-    if(handshake.signedCookies && handshake.signedCookies[key]) handshake.signedCookies[key] = (handshake.signedCookies[key].match(/\:(.*)\./) || []).pop();
-    if(handshake.cookies && handshake.cookies[key]) handshake.cookies[key] = (handshake.cookies[key].match(/\:(.*)\./) || []).pop();
-
-    // original code
+  function findCookie(handshake) {
     return (handshake.secureCookies && handshake.secureCookies[key])
         || (handshake.signedCookies && handshake.signedCookies[key])
         || (handshake.cookies && handshake.cookies[key]);
